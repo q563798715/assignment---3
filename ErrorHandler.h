c6bed5dfc05d7c3d558b057bed956d7b81553fec
q563798715 <zsun@mail.sfsu.edu>
2016-06-20 12:48:37 -0700

    Create ErrorHandler.h

---
+++ ErrorHandler.h
@@ -0,0 +1,29 @@
+#ifndef ERRORHANDLER_H
+#define ERRORHANDLER_H
+class ErrorHandler{
+private:
+    
+    std::fstream outfile;
+    
+public:
+    
+    enum error { error1 = 10 , error2 = 20, error3 = 30};
+    
+    ErrorHandler(char const filename[]) {
+    }
+    
+    ~ErrorHandler(){
+    }
+    
+    std::string errormessage (){
+        return "error" ;
+    }
+    
+    void warn(char const message[], enum error errornum){
+    }
+    
+    
+    void terminate(char const message[] , enum error errornum){
+    }
+};
+#endif
